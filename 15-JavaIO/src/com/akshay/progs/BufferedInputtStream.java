a39e6cf45b977f479435ec902cac187980b77963
Akshay <akhareakshay2@gmail.com>
2021-05-25 20:23:13 +0530

    BufferedInputStream-<Modified>

--- 15-JavaIO/src/com/akshay/progs/BufferedInputtStream.java
+++ 15-JavaIO/src/com/akshay/progs/BufferedInputtStream.java
@@ -11,17 +11,17 @@ public class BufferedInputtStream {
 	public static void main(String[] args) throws IOException {
 
 		FileInputStream fis = new FileInputStream("BufferedInputStream.txt");
-		
+
 		BufferedInputStream bis = new BufferedInputStream(fis);
-		
+
 		int i;
-		
-		while((i=bis.read()) != -1) {
-			System.out.print(i+"....");
-			System.out.println((char)i);
-			
+
+		while ((i = bis.read()) != -1) {
+			System.out.print(i + "....");
+			System.out.println((char) i);
+
 		}
-		
+
 	}
 
 }
